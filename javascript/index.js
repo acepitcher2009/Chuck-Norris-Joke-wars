5db022f0d2469ecfbddf80d3eb2a0d2dac232714
acepitcher2009 <69870798+acepitcher2009@users.noreply.github.com>
2020-10-24 09:03:42 -0500

    Complete Game

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,14 @@
 const gameState = {
     allJokes: [],
     losers: [],
-    winners: []
-
+    winners: [],
+    winnerId: ['win1', 'win2', 'win3', 'win4', 'win5']
 }
 //get chucknorris joke
 
 
 const fetchChuck = async () => {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 6; i++) {
         await fetch('https://api.chucknorris.io/jokes/random').then((r) => {
             return r.json().then((data) => {
                 gameState.allJokes.push(data.value);
@@ -18,24 +18,18 @@ const fetchChuck = async () => {
     }
 
 }
-//fetchChuck();
-//console.log(gameState.allJokes);
+
 
 const joke1 = document.querySelector('#joke-one');
 const joke2 = document.querySelector('#joke-two');
 
 const game = async () => {
 
-    for (let i = 0; i < 2; i++) {
-
+    for (let i = 0; i < 1; i++) {
         await fetchChuck();
-
-
     }
     joke1.innerText = gameState.allJokes[0];
     joke2.innerText = gameState.allJokes[1];
-
-
 }
 
 
@@ -45,7 +39,6 @@ rst.onclick = function () {
     gameState.allJokes = [];
     gameState.winners = [];
     gameState.losers = [];
-    fetchChuck();
     game();
 }
 
@@ -57,47 +50,70 @@ function vote() {
     let vote1 = document.getElementById('vote1');
     let vote2 = document.getElementById('vote2');
 
-    for (let i = 0; i < 10; i++) {
+    // set an onclick function to store the winning selection into an array and to select another random movie for the opponent
+    for (let i = 0; i < 5; i++) {
+
+
         vote1.onclick = function () {
 
-            gameState.winners.push(joke1.textContent);
-            gameState.losers.push(joke2.textContent);
+            sessionStorage.setItem("winner", gameState.winners.push(joke1.textContent));
+            sessionStorage.setItem("loser", gameState.losers.push(joke2.textContent));
             gameState.allJokes.splice(1, 1);
             joke2.textContent = gameState.allJokes[1];
 
         }
 
         vote2.onclick = function () {
-            gameState.winners.push(joke2.textContent);
-            gameState.losers.push(joke1.textContent);
+            sessionStorage.setItem("winner", gameState.winners.push(joke2.textContent));
+            sessionStorage.setItem("loser", gameState.losers.push(joke1.textContent));
             gameState.allJokes.splice(0, 1);
-            
             joke1.textContent = gameState.allJokes[1];
-
-
         }
-
-
 
 
 
     }
 
+}
+
+function showWinner() {
+    let winBtn = document.querySelector('#viewWinners');
+    var x = document.getElementById("toggleWinners");
+
+    winBtn.onclick = function () {
+
+        for (let i = 0; i < 5; i++) {
+            let winJoke = document.getElementById(gameState.winnerId[i]);
+            winJoke.innerHTML = gameState.winners[i];
+        }
+        //console.log(winJoke);
 
 
+        if (toggleWinners.style.display === "none") {
+            toggleWinners.style.display = "block";
+        } else {
+            toggleWinners.style.display = "none";
+        }
+
+    }
 }
 
 
-
-
-
-
-
 vote();
+showWinner();
 
 
 
-// set an onclick function to store the winning selection into an array and to select another random movie for the opponent
+
+
+
+
+
+
+
+
+
+
 
 // set an onclick funstion to skip the movies and display two new movies 
 //set an onlcick function to restart the game by reinitializing the arrays to empty arrays. 
